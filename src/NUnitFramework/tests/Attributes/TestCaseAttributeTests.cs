4b02c3098d5a8304a2b9126263bbf357169a7aaf
Hackle Wayne <hacklew@hotmail.com>
2016-01-20 22:47:37 +1300

    Added dumb tests

--- src/NUnitFramework/tests/Attributes/TestCaseAttributeTests.cs
+++ src/NUnitFramework/tests/Attributes/TestCaseAttributeTests.cs
@@ -27,12 +27,32 @@ using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
 using NUnit.TestData.TestCaseAttributeFixture;
 using NUnit.TestUtilities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework.Compatibility;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
+using System.Collections.Generic;
+using NUnit.Framework.Internal.Builders;
+using System.Reflection;
 
 namespace NUnit.Framework.Attributes
 {
     [TestFixture]
     public class TestCaseAttributeTests
     {
+        [TestCaseDumb]
+        [TestCaseDumb]
+        [TestCaseDumb]
+        public void DumbIntegerDivisionWithResultPassedToTest(int n, int d, int q)
+        {
+            Assert.AreEqual(q, 1);
+            Assert.AreEqual(d, 1);
+            Assert.AreEqual(n, 1);
+        }
+
         [TestCase(12, 3, 4)]
         [TestCase(12, 2, 6)]
         [TestCase(12, 4, 3)]
@@ -486,6 +506,345 @@ namespace NUnit.Framework.Attributes
         public int? TestCaseWithNullableReturnValue(int? a)
         {
             return a;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// TestCaseAttribute is used to mark parameterized test cases
+    /// and provide them with their arguments.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class TestCaseDumbAttribute : NUnitAttribute, ITestBuilder, ITestCaseData, IImplyFixture
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Construct a TestCaseAttribute with a list of arguments.
+        /// This constructor is not CLS-Compliant
+        /// </summary>
+        /// <param name="arguments"></param>
+        public TestCaseDumbAttribute(params object[] arguments)
+        {
+            RunState = RunState.Runnable;
+
+            if (arguments == null)
+                Arguments = new object[] { null };
+            else
+                Arguments = arguments;
+
+            Properties = new PropertyBag();
+        }
+
+        /// <summary>
+        /// Construct a TestCaseAttribute with a single argument
+        /// </summary>
+        /// <param name="arg"></param>
+        public TestCaseDumbAttribute(object arg)
+        {
+            RunState = RunState.Runnable;
+            Arguments = new object[] { arg };
+            Properties = new PropertyBag();
+        }
+
+        /// <summary>
+        /// Construct a TestCaseAttribute with a two arguments
+        /// </summary>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
+        public TestCaseDumbAttribute(object arg1, object arg2)
+        {
+            RunState = RunState.Runnable;
+            Arguments = new object[] { arg1, arg2 };
+            Properties = new PropertyBag();
+        }
+
+        /// <summary>
+        /// Construct a TestCaseAttribute with a three arguments
+        /// </summary>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
+        /// <param name="arg3"></param>
+        public TestCaseDumbAttribute(object arg1, object arg2, object arg3)
+        {
+            RunState = RunState.Runnable;
+            Arguments = new object[] { arg1, arg2, arg3 };
+            Properties = new PropertyBag();
+        }
+
+        #endregion
+
+        #region ITestData Members
+
+        /// <summary>
+        /// Gets or sets the name of the test.
+        /// </summary>
+        /// <value>The name of the test.</value>
+        public string TestName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RunState of this test case.
+        /// </summary>
+        public RunState RunState { get; private set; }
+
+        /// <summary>
+        /// Gets the list of arguments to a test case
+        /// </summary>
+        public object[] Arguments { get; private set; }
+
+        /// <summary>
+        /// Gets the properties of the test case
+        /// </summary>
+        public IPropertyBag Properties { get; private set; }
+
+        #endregion
+
+        #region ITestCaseData Members
+
+        /// <summary>
+        /// Gets or sets the expected result.
+        /// </summary>
+        /// <value>The result.</value>
+        public object ExpectedResult
+        {
+            get { return _expectedResult; }
+            set
+            {
+                _expectedResult = value;
+                HasExpectedResult = true;
+            }
+        }
+        private object _expectedResult;
+
+        /// <summary>
+        /// Returns true if the expected result has been set
+        /// </summary>
+        public bool HasExpectedResult { get; private set; }
+
+        #endregion
+
+        #region Other Properties
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description
+        {
+            get { return Properties.Get(PropertyNames.Description) as string; }
+            set { Properties.Set(PropertyNames.Description, value); }
+        }
+
+        /// <summary>
+        /// The author of this test
+        /// </summary>
+        public string Author
+        {
+            get { return Properties.Get(PropertyNames.Author) as string; }
+            set { Properties.Set(PropertyNames.Author, value); }
+        }
+
+        /// <summary>
+        /// The type that this test is testing
+        /// </summary>
+        public Type TestOf
+        {
+            get { return _testOf; }
+            set
+            {
+                _testOf = value;
+                Properties.Set(PropertyNames.TestOf, value.FullName);
+            }
+        }
+        private Type _testOf;
+
+        /// <summary>
+        /// Gets or sets the reason for ignoring the test
+        /// </summary>
+        public string Ignore
+        {
+            get { return IgnoreReason; }
+            set { IgnoreReason = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="NUnit.Framework.TestCaseAttribute"/> is explicit.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if explicit; otherwise, <c>false</c>.
+        /// </value>
+        public bool Explicit
+        {
+            get { return RunState == RunState.Explicit; }
+            set { RunState = value ? RunState.Explicit : RunState.Runnable; }
+        }
+
+        /// <summary>
+        /// Gets or sets the reason for not running the test.
+        /// </summary>
+        /// <value>The reason.</value>
+        public string Reason
+        {
+            get { return Properties.Get(PropertyNames.SkipReason) as string; }
+            set { Properties.Set(PropertyNames.SkipReason, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the ignore reason. When set to a non-null
+        /// non-empty value, the test is marked as ignored.
+        /// </summary>
+        /// <value>The ignore reason.</value>
+        public string IgnoreReason
+        {
+            get { return Reason; }
+            set
+            {
+                RunState = RunState.Ignored;
+                Reason = value;
+            }
+        }
+
+#if !PORTABLE
+        /// <summary>
+        /// Comma-delimited list of platforms to run the test for
+        /// </summary>
+        public string IncludePlatform { get; set; }
+
+        /// <summary>
+        /// Comma-delimited list of platforms to not run the test for
+        /// </summary>
+        public string ExcludePlatform { get; set; }
+#endif
+
+        /// <summary>
+        /// Gets and sets the category for this test case.
+        /// May be a comma-separated list of categories.
+        /// </summary>
+        public string Category
+        {
+            get { return Properties.Get(PropertyNames.Category) as string; }
+            set
+            {
+                foreach (string cat in value.Split(new char[] { ',' }))
+                    Properties.Add(PropertyNames.Category, cat);
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private TestCaseParameters GetParametersForTestCase(IMethodInfo method)
+        {
+            TestCaseParameters parms;
+
+            try
+            {
+#if NETCF
+                var tmethod = method.MakeGenericMethodEx(Arguments);
+                if (tmethod == null)
+                    throw new NotSupportedException("Cannot determine generic types from probing");
+                method = tmethod;
+#endif
+
+                IParameterInfo[] parameters = method.GetParameters();
+                int argsNeeded = parameters.Length;
+                int argsProvided = Arguments.Length;
+
+                parms = new TestCaseParameters(this);
+
+                parms.Arguments = new object[] { 1, 1, 1 };
+
+                if (argsProvided == argsNeeded)
+                    PerformSpecialConversions(parms.Arguments, parameters);
+            }
+            catch (Exception ex)
+            {
+                parms = new TestCaseParameters(ex);
+            }
+
+            return parms;
+        }
+
+        /// <summary>
+        /// Performs several special conversions allowed by NUnit in order to
+        /// permit arguments with types that cannot be used in the constructor
+        /// of an Attribute such as TestCaseAttribute or to simplify their use.
+        /// </summary>
+        /// <param name="arglist">The arguments to be converted</param>
+        /// <param name="parameters">The ParameterInfo array for the method</param>
+        private static void PerformSpecialConversions(object[] arglist, IParameterInfo[] parameters)
+        {
+            for (int i = 0; i < arglist.Length; i++)
+            {
+                object arg = arglist[i];
+                Type targetType = parameters[i].ParameterType;
+
+                if (arg == null)
+                    continue;
+
+                if (arg is SpecialValue && (SpecialValue)arg == SpecialValue.Null)
+                {
+                    arglist[i] = null;
+                    continue;
+                }
+
+                if (targetType.IsAssignableFrom(arg.GetType()))
+                    continue;
+#if !PORTABLE
+                if (arg is DBNull)
+                {
+                    arglist[i] = null;
+                    continue;
+                }
+#endif
+                bool convert = false;
+
+                if (targetType == typeof(short) || targetType == typeof(byte) || targetType == typeof(sbyte) ||
+                    targetType == typeof(short?) || targetType == typeof(byte?) || targetType == typeof(sbyte?) || targetType == typeof(double?))
+                {
+                    convert = arg is int;
+                }
+                else if (targetType == typeof(decimal) || targetType == typeof(decimal?))
+                {
+                    convert = arg is double || arg is string || arg is int;
+                }
+                else if (targetType == typeof(DateTime) || targetType == typeof(DateTime?))
+                {
+                    convert = arg is string;
+                }
+
+                if (convert)
+                {
+                    Type convertTo = targetType.GetTypeInfo().IsGenericType && targetType.GetGenericTypeDefinition() == typeof(Nullable<>) ?
+                        targetType.GetGenericArguments()[0] : targetType;
+                    arglist[i] = Convert.ChangeType(arg, convertTo, System.Globalization.CultureInfo.InvariantCulture);
+                }
+                else
+                    // Convert.ChangeType doesn't work for TimeSpan from string
+                    if ((targetType == typeof(TimeSpan) || targetType == typeof(TimeSpan?)) && arg is string)
+                    {
+                        arglist[i] = TimeSpan.Parse((string)arg);
+                    }
+            }
+        }
+        #endregion
+
+        #region ITestBuilder Members
+
+        /// <summary>
+        /// Construct one or more TestMethods from a given MethodInfo,
+        /// using available parameter data.
+        /// </summary>
+        /// <param name="method">The MethodInfo for which tests are to be constructed.</param>
+        /// <param name="suite">The suite to which the tests will be added.</param>
+        /// <returns>One or more TestMethods</returns>
+        public IEnumerable<TestMethod> BuildFrom(IMethodInfo method, Test suite)
+        {
+            TestMethod test = new NUnitTestCaseBuilder().BuildTestMethod(method, suite, GetParametersForTestCase(method));
+
+            yield return test;
         }
 
         #endregion
